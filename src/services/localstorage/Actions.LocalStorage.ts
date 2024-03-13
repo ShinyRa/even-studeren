a03a5aa7bca15cd3e0ac0b3f39b88faf2724e2a0
Auke Steenman <auke.steenman@gmail.com>
2024-03-13 18:55:09 +0100

    Wip: Removed localStorage var and just directly use the localStorage object

--- src/services/localstorage/Actions.LocalStorage.ts
+++ src/services/localstorage/Actions.LocalStorage.ts
@@ -3,27 +3,27 @@ import LocalStorageService from './LocalStorage.service'
 class ActionsLocalStorage extends LocalStorageService { 
 
     static add(key: string, value: string) { 
-        this.database.getItem(key) === null
-            ? this.database.setItem(key, value)
+        localStorage.getItem(key) === null
+            ? localStorage.setItem(key, value)
             : console.log(`Key '${key}' is already in localStorage.`);
     }
 
     static update(key: string, value: string) { 
-        this.database.getItem(key)
-            ? this.database.setItem(key, value)
+        localStorage.getItem(key)
+            ? localStorage.setItem(key, value)
             : console.log(`Key '${key}' does not exist in localStorage.`);
 
     }
 
     static remove(key: string) { 
-        this.database.getItem(key)
-        ? this.database.removeItem(key)
+        localStorage.getItem(key)
+        ? localStorage.removeItem(key)
         : console.log(`Key '${key}' does not exist in localStorage.`);
 
     }
 
     static get(key: string): string | null { 
-        const item = this.database.getItem(key)
+        const item = localStorage.getItem(key)
         return item ? item : null
     }
 
--- src/services/localstorage/LocalStorage.service.ts
+++ src/services/localstorage/LocalStorage.service.ts
@@ -1,5 +1,5 @@
-abstract class LocalStorageService { 
-    static database: Storage = localStorage
+class LocalStorageService { 
+    //static database: Storage = localStorage
 
     // TODO: populate localstorage with existing data function
 }
