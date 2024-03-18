86284c4a3a9e34d7091eb7aa3b92a8accc08b4fb
tijs <tijs.de.graaff@hva.nl>
2024-03-18 16:13:39 +0100

    feat: Flippable quizcard

--- src/components/quizcard/QuizCard.tsx
+++ src/components/quizcard/QuizCard.tsx
@@ -1,31 +1,68 @@
 import { QuizCardType } from "./QuizCardType";
-import './QuizCard.scss'
+import { useState } from "react";
+import { CSSTransition } from 'react-transition-group' 
+
+import './quizcard.scss'
+import './flip-transition.scss'
 
 const QuizCard = ({card}: QuizCardType) => {
 
+    const [showFront, setShowFront] = useState(true)
+
     return (
-        <div className="card">
-            <div className="card-content">
-                <p className="title">
-                    {card.questionTitle}
-                </p>
-                <p className="subtitle">
-                    {card.questionDescription}
-                </p>
-            </div>
-            <footer className="card-footer">
-                <p className="card-footer-item">
-                    {card.tags.map(({identifier}) => (
-                        <span className="tag is-info">{identifier}</span>
-                    ))}
-                </p>
-                <p className="card-footer-item">
-                    {card.categories.map(({identifier}) => (
-                        <span className="tag is-danger">{identifier}</span>
-                    ))}
-                </p>
-            </footer>
+        <CSSTransition
+            in={showFront}
+            timeout={300}
+            classNames={'flip'}
+        >
+            <div className="flippable" onClick={() => setShowFront(!showFront)}>
+                <div className="card card-front">
+                    <div className="card-content">
+                        <p className="title">
+                            {card.questionTitle}
+                        </p>
+                        <p className="subtitle">
+                            {card.questionDescription}
+                        </p>
+                    </div>
+                    <footer className="card-footer">
+                        <p className="card-footer-item">
+                            {card.tags.map(({identifier}) => (
+                                <span className="tag is-info" key={identifier}>{identifier}</span>
+                            ))}
+                        </p>
+                        <p className="card-footer-item">
+                            {card.categories.map(({identifier}) => (
+                                <span className="tag is-danger" key={identifier}>{identifier}</span>
+                            ))}
+                        </p>
+                    </footer>
+                </div>
+                <div className="card card-back">
+                    <div className="card-content">
+                        <p className="title">
+                            {card.answerTitle}
+                        </p>
+                        <p className="subtitle">
+                            {card.answerDescription}
+                        </p>
+                    </div>
+                    <footer className="card-footer">
+                        <p className="card-footer-item">
+                            {card.tags.map(({identifier}) => (
+                                <span className="tag is-info" key={identifier}>{identifier}</span>
+                                ))}
+                        </p>
+                        <p className="card-footer-item">
+                            {card.categories.map(({identifier}) => (
+                                <span className="tag is-danger" key={identifier}>{identifier}</span>
+                                ))}
+                        </p>
+                    </footer>
+                </div>
         </div>
+        </CSSTransition>
+
     )
 }
 
